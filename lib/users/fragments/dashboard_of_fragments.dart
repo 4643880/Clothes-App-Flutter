9614332898097f990421d6140da4cb2a4b4122c4
Aizaz Haider <aizazhaider720@gmail.com>
2022-10-22 11:36:13 +0500

    Implemented Animated Bottom Navigation Bar Of Dashboard

--- lib/users/fragments/dashboard_of_fragments.dart
+++ lib/users/fragments/dashboard_of_fragments.dart
@@ -1,10 +1,10 @@
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:clothes_app/users/fragments/favorite_fragment_screen.dart';
 import 'package:clothes_app/users/fragments/home_fragment_screen.dart';
 import 'package:clothes_app/users/fragments/order_fragment_screen.dart';
 import 'package:clothes_app/users/fragments/profile_fragment_screen.dart';
 import 'package:clothes_app/users/userPreferences/current_user.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 
 class DashboardOfFragments extends StatelessWidget {
@@ -20,26 +20,28 @@ class DashboardOfFragments extends StatelessWidget {
 
   List navigationButtonsProperties = [
     {
-      "active_icon" : Icons.home,
-      "none_active_icon" : Icons.home_outlined,
+      "active_icon" : const Icon(Icons.home),
+      "none_active_icon" : const Icon(Icons.home_outlined),
       "label" : "Home"
     },
     {
-      "active_icon" : Icons.favorite,
-      "none_active_icon" : Icons.favorite_outline,
+      "active_icon" : const Icon(Icons.favorite),
+      "none_active_icon" : const Icon(Icons.favorite_outline),
       "label" : "Favorites"
     },
     {
-      "active_icon" : FontAwesomeIcons.boxOpen,
-      "none_active_icon" : FontAwesomeIcons.box,
+      "active_icon" : const FaIcon(FontAwesomeIcons.boxOpen),
+      "none_active_icon" : const FaIcon(FontAwesomeIcons.box),
       "label" : "Orders"
     },
     {
-      "active_icon" : Icons.person,
-      "none_active_icon" : Icons.person_outline,
+      "active_icon" : const Icon(Icons.person),
+      "none_active_icon" : const Icon(Icons.person_outline),
       "label" : "Profile"
     },
   ];
+
+  Rx<int> indexNumber = 0.obs;
 
   @override
   Widget build(BuildContext context) {
@@ -50,7 +52,36 @@ class DashboardOfFragments extends StatelessWidget {
       },
       builder: (controller) {
         return Scaffold(
-          appBar: AppBar(title: const Text("Dashboard Fragment screen"),),
+          backgroundColor: Colors.black,
+          appBar: AppBar(title: const Text("Dashboard Fragment screen")),
+          body: SafeArea(
+            child: Obx(
+              () {
+                return fragmentScreens[indexNumber.value];
+              },
+            ),
+          ),
+          bottomNavigationBar: Obx(
+            () => BottomNavigationBar(
+              currentIndex: indexNumber.value,
+              onTap: (value) {
+                print(value);
+                indexNumber.value = value;
+              },
+              showSelectedLabels: true,
+              showUnselectedLabels: true,
+              selectedItemColor: Colors.white,
+              unselectedItemColor: Colors.white24,
+              items: List.generate(4, (index) {
+                return BottomNavigationBarItem(
+                  backgroundColor: Colors.black,
+                  icon: navigationButtonsProperties[index]["none_active_icon"],
+                  activeIcon: navigationButtonsProperties[index]["active_icon"],
+                  label: navigationButtonsProperties[index]["label"]
+                );
+              }),
+            ),
+          ),
         );
       },
     );

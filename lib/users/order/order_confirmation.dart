baffe45fc12f05a0feec9d6ca276df4fa7113408
Aizaz Haider <aizazhaider720@gmail.com>
2023-02-08 16:32:33 +0500

    Designed Order Confirmation Screen

--- lib/users/order/order_confirmation.dart
+++ lib/users/order/order_confirmation.dart
@@ -1,4 +1,11 @@
+import 'dart:typed_data';
+import 'package:clothes_app/users/userPreferences/current_user.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+
 
 class OrderConfirmationScreen extends StatelessWidget {
   final List<int>? selectedCartIDs;
@@ -10,7 +17,7 @@ class OrderConfirmationScreen extends StatelessWidget {
   final String? shipmentAddress;
   final String? note;
 
-  const OrderConfirmationScreen({
+  OrderConfirmationScreen({
     Key? key,
     this.selectedCartIDs,
     this.selectedCartListItemsInfo,
@@ -22,6 +29,44 @@ class OrderConfirmationScreen extends StatelessWidget {
     this.note,
   }) : super(key: key);
 
+
+
+  RxList<int> _imageSelectedByte = <int>[].obs;
+  Uint8List get imageSelectedByte => Uint8List.fromList(_imageSelectedByte);
+
+  RxString _imageSelectedName = "".obs;
+  String get imageSelectedName => _imageSelectedName.value;
+
+  final ImagePicker _picker = ImagePicker();
+
+  CurrentUserState currentUser = Get.put(CurrentUserState());
+
+
+  setSelectedImage(Uint8List selectedImage)
+  {
+    _imageSelectedByte.value = selectedImage;
+  }
+
+  setSelectedImageName(String selectedImageName)
+  {
+    _imageSelectedName.value = selectedImageName;
+  }
+
+  chooseImageFromGallery() async
+  {
+    final pickedImageXFile = await _picker.pickImage(source: ImageSource.gallery);
+
+    if(pickedImageXFile != null)
+    {
+      final bytesOfImage = await pickedImageXFile.readAsBytes();
+
+      // Assigning to State Management
+      setSelectedImage(bytesOfImage);
+      setSelectedImageName(path.basename(pickedImageXFile.path));
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,7 +74,107 @@ class OrderConfirmationScreen extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [],
+          children: [
+            //image
+            Image.asset(
+              "images/transaction.png",
+              width: 160,
+            ),
+            const SizedBox(height: 4,),
+
+            //title
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                "Please Attach Transaction \nProof Screenshot / Image",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white70,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 30),
+
+            //select image btn
+            Material(
+              elevation: 8,
+              color: Colors.purpleAccent,
+              borderRadius: BorderRadius.circular(30),
+              child: InkWell(
+                onTap: ()
+                {
+                  chooseImageFromGallery();
+                },
+                borderRadius: BorderRadius.circular(30),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 30,
+                    vertical: 12,
+                  ),
+                  child: Text(
+                    "Select Image",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 16),
+
+            //display selected image by user
+            Obx(()=> ConstrainedBox(
+              constraints: BoxConstraints(
+                maxWidth: MediaQuery.of(context).size.width * 0.7,
+                maxHeight: MediaQuery.of(context).size.width * 0.6,
+              ),
+              child: imageSelectedByte.length > 0
+                  ? Image.memory(imageSelectedByte, fit: BoxFit.contain,)
+                  : const Placeholder(color: Colors.white60,),
+            )),
+
+            const SizedBox(height: 16),
+
+            //confirm and proceed
+            Obx(()=> Material(
+              elevation: 8,
+              color: imageSelectedByte.length > 0 ? Colors.purpleAccent : Colors.grey,
+              borderRadius: BorderRadius.circular(30),
+              child: InkWell(
+                onTap: ()
+                {
+                  if(imageSelectedByte.length > 0)
+                  {
+                    //save order info
+
+                  }
+                  else
+                  {
+                    Fluttertoast.showToast(msg: "Please attach the transaction proof / screenshot.");
+                  }
+                },
+                borderRadius: BorderRadius.circular(30),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 30,
+                    vertical: 12,
+                  ),
+                  child: Text(
+                    "Confirmed & Proceed",
+                    style: TextStyle(
+                      color: Colors.white70,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ),
+            )),
+          ],
         ),
       ),
     );
